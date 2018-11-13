4f610a77b79efcd019d48c0751c349f3a9d4a977
Mihai Nicolae <nicolae@ca.ibm.com>
2018-11-12 23:24:51 -0500

    add upload time report UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactTable from "react-table";
 import "react-table/react-table.css";
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      payrolls: []
+      selectedFile: null,
+      payrolls: [],
+      loaded: 0,
     };
   }
 
@@ -16,6 +19,30 @@ class App extends Component {
     fetch('/v1/payroll/getPayrollReport')
       .then(response => response.json())
       .then(data => this.setState({ payrolls: data.payrolls }))
+  }
+
+  handleselectedFile = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  handleUpload = () => {
+    const data = new FormData()
+    data.append('timeReportFile', this.state.selectedFile, this.state.selectedFile.name)
+
+    axios
+      .post('/v1/payroll/uploadTimeReport', data, {
+        onUploadProgress: ProgressEvent => {
+          this.setState({
+            loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
+          })
+        },
+      })
+      .then(res => {
+        console.log(res.statusText)
+      })
   }
 
 render() {
@@ -43,6 +70,8 @@ render() {
           className="-striped -highlight"
         />
         <br />
+        <input type="file" name="" id="" onChange={this.handleselectedFile} />
+        <button onClick={this.handleUpload}>Upload</button>
       </div>
     );
   }
